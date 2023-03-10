7649bb3ed21c35e182b423f97b214cc690a590a3
Sowon Lee <sowon7191@pionnet.co.kr>
2023-03-10 16:25:06 +0900

    commit

---
+++ Example/Pods/Target Support Files/LogViewLibrary/LogViewLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LogViewLibrary : NSObject
+@end
+@implementation PodsDummy_LogViewLibrary
+@end
---
+++ Example/Pods/Target Support Files/Pods-LogViewLibrary_Tests/Pods-LogViewLibrary_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LogViewLibrary_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LogViewLibrary_Tests
+@end
